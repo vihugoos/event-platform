647c653378928a23568d4f37ee373c9ba1f7e08d
Victor Hugo <victorhugoos@live.com>
2022-06-27 15:14:03 -0300

    created 'subscribe' page and adjusts in 'lesson'

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format } from 'date-fns'
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom'
+import classNames from 'classnames'
 
 interface LessonProps {
     title: string;
@@ -10,72 +11,62 @@ interface LessonProps {
 }
 
 export function Lesson(props: LessonProps) {
+    const { slug } = useParams<{ slug: string }>()
+
     const isLessonAvailable = isPast(props.availableAt)
+    const isActiveLesson = slug === props.slug
     const availableDateFormatted = format(props.availableAt, "EEEE' • 'd' 'MMMM' • 'k'h'mm")
 
     return (
-        <>
-            {isLessonAvailable ? (
-                <Link to={`/event/lesson/${props.slug}`} className="group">
-                    <span className="text-gray-300"> 
-                        {availableDateFormatted}
-                    </span>
+        <Link to={`/event/lesson/${props.slug}`} className={classNames("group", {
+            'pointer-events-none': !isLessonAvailable
+        })}>
+            <span className="text-gray-300"> 
+                {availableDateFormatted}
+            </span>
 
-                    <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
-                        <header className="flex items-center justify-between">
-                            <span className="text-sm text-blue-500 font-medium flex items-center gap-2"> 
-                                <CheckCircle size={20} />
-                                Released
-                            </span>
+            <div 
+                className={classNames("rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500", {
+                    "bg-green-500": isActiveLesson
+                })}
+            >
+                <header className="flex items-center justify-between">
+                    {isLessonAvailable ? (
+                        <span className={classNames("text-sm font-medium flex items-center gap-2", {
+                            "text-white": isActiveLesson,
+                            "text-blue-500": !isActiveLesson
+                        })}>
+                            <CheckCircle size={20} />
+                            Released
+                        </span>
+                    ) : (
+                        <span className="text-sm text-orange-500 font-medium flex items-center gap-2"> 
+                            <Lock size={20} />
+                            Coming soon 
+                        </span>
+                    )}
+                    
+                    {props.type === 'live' ? (
+                        <span className="text-xs rounded py-[0.125rem] px-2 text-white font-bold bg-red-600"> 
+                            LIVE
+                        </span>
+                    ) : (
+                        <span className={classNames("text-xs rounded py-[0.125rem] px-2 text-white border font-bold", {
+                            "border-white": isActiveLesson,
+                            "border-green-300": !isActiveLesson
+                        })}> 
+                            PRACTICAL CLASS 
+                        </span>
+                    )}
+                </header>
 
-                            {props.type === 'live' 
-                            ? 
-                            <span className="text-xs rounded py-[0.125rem] px-2 text-white font-bold bg-red-600"> 
-                                LIVE
-                            </span>
-                            : 
-                            <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold"> 
-                                PRACTICAL CLASS 
-                            </span>
-                            }
-                        </header>
-
-                        <strong className="text-gray-200 mt-5 block"> 
-                            {props.title}
-                        </strong>
-                    </div>
-                </Link>
-            ): (
-                <Link to={`/event/lesson/${props.slug}`} className="pointer-events-none group">
-                    <span className="text-gray-300 hover:cursor-not-allowed"> 
-                        {availableDateFormatted}
-                    </span>
-
-                    <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-red-400 group-hover:cursor-not-allowed">
-                        <header className="flex items-center justify-between">
-                            <span className="text-sm text-orange-500 font-medium flex items-center gap-2"> 
-                                <Lock size={20} />
-                                Coming soon 
-                            </span>
-
-                            {props.type === 'live' 
-                            ? 
-                            <span className="text-xs rounded py-[0.125rem] px-2 text-white font-bold bg-red-600"> 
-                                LIVE
-                            </span>
-                            : 
-                            <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold"> 
-                                PRACTICAL CLASS 
-                            </span>
-                            }
-                        </header>
-
-                        <strong className="text-gray-200 mt-5 block"> 
-                            {props.title}
-                        </strong>
-                    </div>
-                </Link>
-            )} 
-        </>
+                <strong className={classNames("mt-5 block", {
+                    "text-white": isActiveLesson,
+                    "text-gray-200": !isActiveLesson
+                })}>
+                    {props.title}
+                </strong>
+            </div>
+        </Link>
     )
 }
---
+++ src/pages/Subscribe.tsx
@@ -0,0 +1,85 @@
+import { gql, useMutation } from '@apollo/client'
+import { useState, FormEvent} from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Logo } from '../components/Logo'
+
+const CREATE_SUBSCRIBE_MUTATION = gql `
+    mutation CreateSubscriber ($name: String!, $email: String!) {
+        createSubscriber(data: {name: $name, email: $email}) {
+            id
+        }
+    }
+`
+
+export function Subscribe() {
+    const navigate = useNavigate()
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+
+    const [ createSubscriber, { loading } ] = useMutation(CREATE_SUBSCRIBE_MUTATION)
+
+    async function handleSubscribe(event: FormEvent) {
+        event.preventDefault()
+
+        await createSubscriber({
+            variables: {
+                name,
+                email
+            }
+        })
+
+        navigate('/event')
+    }
+
+    return (
+        <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+            
+            <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 max-auto">
+                <div className="max-w-[640px]"> 
+                    <Logo />
+
+                    <h1 className="mt-8 text-[2.5rem] leading-light"> 
+                        Build a <strong className="text-blue-500"> complete application </strong>, from scratch, with <strong className="text-blue-500"> React. </strong> 
+                    </h1>
+
+                    <p className="mt-4 text-gray-200 leading-relaxed">
+                        In just one week you will build a complete application using the most demanded technologies on the market. To access the best opportunities.
+                    </p>
+                </div>
+
+                <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+                    <strong className="text-2xl mb-6 block"> 
+                        Sign up for free
+                    </strong>
+
+                    <form  onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
+                        <input 
+                            type="text" 
+                            placeholder="Your complete name" 
+                            className="bg-gray-900 rounded px-5 h-14" 
+                            onChange={event => setName(event.target.value)}
+                        />
+
+                        <input 
+                            type="email" 
+                            placeholder="Your address email" 
+                            className="bg-gray-900 rounded px-5 h-14" 
+                            onChange={event => setEmail(event.target.value)}
+                        /> 
+
+                        <button 
+                            type="submit"
+                            disabled={loading}
+                            className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
+                        >
+                            Secure my spot
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+            <img src="/src/assets/code_mockup.png" className="mt-10" alt=""/>
+        </div>
+    )
+}
